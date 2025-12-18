c441fd3cd2b618ba52b2ddba77b3c8fdb5078a77
Mallor <messiasjuniormj705@gmail.com>
2025-12-17 22:52:24 -0300

    fix-version-and-description

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="multiscope",
-    version="0.5.0",
-    description="A tool to launch multiple instances of Steam using Gamescope.",
+    version="0.6.1",
+    description="MultiScope is an open-source tool for Linux that enables the creation and management of gamescope sessions of steam, allowing several players to play simultaneously on a single computer",
     author="Mallor705",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
